2222a9a5db33b5c060314bf076ff4511427f0464
Oskar Bergman <ocke_bergman@hotmail.com>
2024-08-23 09:03:16 +0200

    Removed unused method, set int hour to final as it never changes.

--- src/main/java/se/lernia/lab/HourlyPrice.java
+++ src/main/java/se/lernia/lab/HourlyPrice.java
@@ -1,25 +1,26 @@
 package se.lernia.lab;
 
-public class HourlyPrice implements Comparable<HourlyPrice>{
-    private int hour;
+public class HourlyPrice implements Comparable<HourlyPrice> {
+    private final int hour;
     private int price;
 
     public HourlyPrice(int hour, int price) {
         this.hour = hour;
         this.price = price;
     }
+
     public int getHour() {
         return hour;
     }
+
     public int getPrice() {
         return price;
     }
-    public void setPrice(int price) {
-        this.price = price;
-    }
+
     public String toString() {
         return "Timme " + hour + "-" + (hour + 1) + ": " + price + " öre";
     }
+
     public int compareTo(HourlyPrice other) {
         return Integer.compare(this.price, other.price);
     }
